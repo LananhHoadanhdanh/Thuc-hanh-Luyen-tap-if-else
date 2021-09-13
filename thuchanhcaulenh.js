379782cf034373034a108554b2068592863a233a
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2021-09-13 10:49:39 +0700

    xong

---
+++ thuchanhcaulenh.js
@@ -0,0 +1,10 @@
+
+function sent () {
+let answer = document.getElementById("traloi").value;
+if (answer === "ECMAScript") {
+    alert("RIGHT");
+}
+else {
+    alert("Didn’t know? ECMAScript!")
+}
+}
